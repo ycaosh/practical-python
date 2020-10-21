356b64a795e9d872d7b01dee21b01572d266ccdc
ycaosh <ycao@163.com>
2020-10-21 23:39:19 +0800

    exercise 1.30 finished

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
-cost = 0
-with open('Work/Data/portfolio.csv', 'rt') as f:
-    next(f)
-    for line in f:
-        row = line.strip().split(',')
-        cost += int(row[1]) * float(row[2])
+def portfolio_cost(filename):
+    cost = 0
+    with open(filename, 'rt') as f:
+        next(f)
+        for line in f:
+            row = line.strip().split(',')
+            cost += int(row[1]) * float(row[2])
+    return cost
+
+
+cost = portfolio_cost('work/data/portfolio.csv')
 print(f'Total cost {cost:.2f}')
