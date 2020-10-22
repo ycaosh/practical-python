0c916630187c8a7e5c4d643bbf0242ccd75523b0
ycaosh <ycao@163.com>
2020-10-22 16:45:52 +0800

    exercise 2.6 finished

--- Work/report.py
+++ Work/report.py
@@ -17,3 +17,13 @@ def read_portfolio(filename):
             holding['price'] = float(row[2])
             portfolio.append(holding)
     return portfolio
+
+
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if len(row) != 0:
+                prices[row[0]] = float(row[1])
+    return prices
