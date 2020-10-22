ce9ca76a561a1ff304dd00114220e152aefd04ea
ycaosh <ycao@163.com>
2020-10-22 15:05:13 +0800

    exercise 2.4 finished

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,16 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+
+def read_portfolio(filename):
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
