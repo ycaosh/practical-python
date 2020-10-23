99da65105ddecbc86b0bb64fe2f1d1fba13f93ec
ycaosh <ycao@163.com>
2020-10-24 00:56:59 +0800

    exercise 2.15 finished

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,21 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+
+
 def portfolio_cost(filename):
     cost = 0
     with open(filename, 'rt') as f:
-        next(f)
-        for line in f:
-            row = line.strip().split(',')
-            cost += int(row[1]) * float(row[2])
+        rows = csv.reader(f)
+        next(rows)
+        for rowno, row in enumerate(rows, start=1):
+            try:
+                cost += int(row[1]) * float(row[2])
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
     return cost
 
 
-cost = portfolio_cost('work/data/portfolio.csv')
+cost = portfolio_cost('data/portfolio.csv')
 print(f'Total cost {cost:.2f}')
