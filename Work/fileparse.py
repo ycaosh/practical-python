da3dbfc4a1d6f0955f809d86812d6fca10a01e4c
ycaosh <ycao@163.com>
2020-11-05 22:41:21 +0800

    exercise 3.4 finished

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,21 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+
+def parse_csv(filename, select=None):
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            if select:
+                record = {header: data for header, data in zip(
+                    headers, row) if header in select}
+            else:
+                record = dict(zip(headers, row))
+            records.append(record)
+    return records
