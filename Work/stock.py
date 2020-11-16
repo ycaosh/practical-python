3e3db83d4a1d71dd5a483da47c8b9ee7dc46c689
ycaosh <ycao@163.com>
2020-11-16 11:06:45 +0800

    exercise 5.8 finished

--- Work/stock.py
+++ Work/stock.py
@@ -1,8 +1,20 @@
 class Stock:
+    __slots__ = ('name',  'price', '_shares')
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('expected an integer')
+        self._shares = value
 
     def cost(self):
         return self.shares * self.price
